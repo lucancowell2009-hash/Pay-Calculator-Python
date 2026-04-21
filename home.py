83d7d12e307d65445638d383ad83f588232431ca
lucancowell2009-hash <lucan.cowell2009@gmail.com>
2026-04-21 21:50:38 +1000

    Change user input prompts from 'yes/no' to 'y/n'

--- home.py
+++ home.py
@@ -1,29 +1,29 @@
 reg_hours = float(input('Enter number of regular paid hours: '))
 pay = float(input('Enter hourly pay ($): '))
-breaks_check = input('Did you take any breaks? (yes/no): ')
+breaks_check = input('Did you take any breaks? (y/n): ')
 
-if breaks_check.lower() == 'yes':
+if breaks_check.lower() == 'y':
     breaks = float(input('Enter time taken for breaks (minutes): '))
 
 else:
     breaks = 0
 
-late_hours_check = input('Did you work late hours? (yes/no): ')
-if late_hours_check.lower() == 'yes':
+late_hours_check = input('Did you work late hours? (y/n): ')
+if late_hours_check.lower() == 'y':
     late_hours = float(input('Enter number of late hours: '))
 
 else:
     late_hours = 0
 
-over_hours_check = input('Did you work overtime hours? (yes/no): ')
-if over_hours_check.lower() == 'yes':
+over_hours_check = input('Did you work overtime hours? (y/n): ')
+if over_hours_check.lower() == 'y':
     over_hours = float(input('Enter number of overtime hours: '))
 
 else:
     over_hours = 0    
     
-weekend_check = input('Did you work on the weekend? (yes/no): ')
-if weekend_check.lower() == 'yes':
+weekend_check = input('Did you work on the weekend? (y/n): ')
+if weekend_check.lower() == 'y':
     weekend_hours = float(input('Enter number of regular hours worked on the weekend: '))
 
 else:
